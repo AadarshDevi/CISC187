827dabfc8c342ce6ff9fefb7ee6b02f2c6a7e666
Aadarsh D <aadarsh123456char@gmail.com>
2026-08-30 22:20:01 -0700

    init commit

---
+++ cisc181/week1/Week1_Arrays.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int main() {
+
+	// create an array containing 100 ints
+	int array[100];
+
+	// size of an int in C++
+	std::cout << "Size of an int (bytes): " << sizeof(array[0]) << "\n";
+}
+
+/*
+ * Array in C++: https://www.geeksforgeeks.org/cpp/cpp-arrays/
+ * Size of int: https://www.geeksforgeeks.org/cpp/cpp-sizeof-operator/
+ */
