36ec6f87505117dfd0f7eade31bd5093688ed63e
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-06 14:53:44 -0700

    replaced std::vector with array

--- cisc181/week2/Week2_Searching.cpp
+++ cisc181/week2/Week2_Searching.cpp
@@ -1,62 +1,61 @@
 #include <iostream>
-#include <vector>
 
-int linear_search(int search_num, int &vec);
+int linear_search(int search_num, int *arr, const int ARRAY_SIZE);
 
-int binary_search(int search_num, int &vec);
+int binary_search(int search_num, int *arr, const int ARRAY_SIZE);
 
-void print_search(int search_num_i, int search_num_i1);
+void print_search(int num, int num_index);
 
 int main() {
     const int ARRAY_SIZE = 7;
-    int vec[ARRAY_SIZE] = {2, 4, 6, 8, 10, 12, 13};
+    int arr[ARRAY_SIZE] = {2, 4, 6, 8, 10, 12, 13};
 
     // Linear Search
     std::cout << "Linear Search\n";
     std::cout << "-----------------------\n";
-    std::cout << "vector:";
+    std::cout << "array:";
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        std::cout << " " << vec[i];
+        std::cout << " " << arr[i];
     }
     std::cout << "\n\n";
 
     int search_num = 2;
-    int search_num_i = linear_search(search_num, vec);
+    int search_num_i = linear_search(search_num, arr, ARRAY_SIZE);
     print_search(search_num, search_num_i);
 
     search_num = 18;
-    search_num_i = linear_search(search_num, vec);
+    search_num_i = linear_search(search_num, arr, ARRAY_SIZE);
     print_search(search_num, search_num_i);
 
     search_num = 13;
-    search_num_i = linear_search(search_num, vec);
+    search_num_i = linear_search(search_num, arr, ARRAY_SIZE);
     print_search(search_num, search_num_i);
 
     // Binary Search
     std::cout << "\n\nBinary Search\n";
     std::cout << "-----------------------\n";
-    std::cout << "vector:";
+    std::cout << "array:";
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        std::cout << " " << vec[i];
+        std::cout << " " << arr[i];
     }
     std::cout << "\n\n";
 
     search_num = 2;
-    search_num_i = binary_search(search_num, vec);
+    search_num_i = binary_search(search_num, arr, ARRAY_SIZE);
     print_search(search_num, search_num_i);
 
     search_num = 18;
-    search_num_i = binary_search(search_num, vec);
+    search_num_i = binary_search(search_num, arr, ARRAY_SIZE);
     print_search(search_num, search_num_i);
 
     search_num = 13;
-    search_num_i = binary_search(search_num, vec);
+    search_num_i = binary_search(search_num, arr, ARRAY_SIZE);
     print_search(search_num, search_num_i);
 
     return 0;
 }
 
-int binary_search(int search_num, int &vec, const int ARRAY_SIZE) {
+int binary_search(int search_num, int *arr, const int ARRAY_SIZE) {
     int start_i = 0;
     int end_i = ARRAY_SIZE - 1;
     int mid_i = -1;
@@ -64,7 +63,7 @@ int binary_search(int search_num, int &vec, const int ARRAY_SIZE) {
 
     while (start_i <= end_i) {
         mid_i = (start_i + end_i) / 2;
-        mid_num = vec[mid_i];
+        mid_num = arr[mid_i];
 
         if (search_num == mid_num) {
             return mid_i;
@@ -79,9 +78,9 @@ int binary_search(int search_num, int &vec, const int ARRAY_SIZE) {
     return -1;
 }
 
-int linear_search(int search_num, int &vec, const int ARRAY_SIZE) {
+int linear_search(int search_num, int *arr, const int ARRAY_SIZE) {
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        if (vec[i] == search_num) {
+        if (arr[i] == search_num) {
             return i;
         }
     }
