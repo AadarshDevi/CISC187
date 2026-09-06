b56e43cabe98d7ae582f27bd6dbb6a28b152e2d8
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-06 12:45:34 -0700

    printing the num and index

--- cisc181/week2/Week2_Searching.cpp
+++ cisc181/week2/Week2_Searching.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <vector>
 
+
+void print_search(int search_num_i, int search_num_i1);
+
 int main() {
     std::cout << "Hello World!\n";
 
     std::vector<int> vec = {2, 4, 6, 8, 10, 12, 13};
 
     return 0;
+}
+
+
+void print_search(int num, int num_index) {
+    std::cout << "Number " << num << " @ index " << num_index << ".\n";
 }
