d667e85e58c2ab2f4a0b8fec75f4120f7f9a33b4
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-06 16:22:25 -0700

    old code commented for later use

--- cisc181/week2/Week2_Searching.cpp
+++ cisc181/week2/Week2_Searching.cpp
@@ -94,3 +94,50 @@ int linear_search(int search_num, int *arr, const int ARRAY_SIZE) {
 void print_search(int num, int num_index) {
     std::cout << "Number " << num << " @ index " << num_index << ".\n";
 }
+
+/*
+const int ARRAY_SIZE = 7;
+    int arr[ARRAY_SIZE] = {2, 4, 6, 8, 10, 12, 13};
+
+    // Linear Search
+    std::cout << "Linear Search\n";
+    std::cout << "-----------------------\n";
+    std::cout << "array:";
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        std::cout << " " << arr[i];
+    }
+    std::cout << "\n\n";
+
+    int search_num = 8;
+    int search_num_i = linear_search(search_num, arr, ARRAY_SIZE);
+    print_search(search_num, search_num_i);
+
+    search_num = 18;
+    search_num_i = linear_search(search_num, arr, ARRAY_SIZE);
+    print_search(search_num, search_num_i);
+
+    search_num = 13;
+    search_num_i = linear_search(search_num, arr, ARRAY_SIZE);
+    print_search(search_num, search_num_i);
+
+    // Binary Search
+    std::cout << "\n\nBinary Search\n";
+    std::cout << "-----------------------\n";
+    std::cout << "array:";
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        std::cout << " " << arr[i];
+    }
+    std::cout << "\n\n";
+
+    search_num = 2;
+    search_num_i = binary_search(search_num, arr, ARRAY_SIZE);
+    print_search(search_num, search_num_i);
+
+    search_num = 18;
+    search_num_i = binary_search(search_num, arr, ARRAY_SIZE);
+    print_search(search_num, search_num_i);
+
+    search_num = 13;
+    search_num_i = binary_search(search_num, arr, ARRAY_SIZE);
+    print_search(search_num, search_num_i);
+ */
