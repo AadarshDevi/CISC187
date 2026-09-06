db90b6084b2fa05a3bc6b05fb914282ab0dac2e2
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-06 12:46:35 -0700

    printing hello world

--- cisc181/week2/Week2_Searching.cpp
+++ cisc181/week2/Week2_Searching.cpp
@@ -8,6 +8,15 @@ void print_search(int search_num_i, int search_num_i1);
 int main() {
     std::vector<int> vec = {2, 4, 6, 8, 10, 12, 13};
 
+    std::cout << "Linear Search\n";
+    std::cout << "-----------------------\n";
+    std::cout << "vector:";
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << " " << vec[i];
+    }
+    std::cout << "\n\n";
+
+
     return 0;
 }
 
