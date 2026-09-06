535ffd2ba8c78859568a6b49deb934453604ea15
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-06 12:46:08 -0700

    linear search function and function prototype

--- cisc181/week2/Week2_Searching.cpp
+++ cisc181/week2/Week2_Searching.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+int linear_search(int search_num, std::vector<int> &vec);
 
 void print_search(int search_num_i, int search_num_i1);
 
@@ -12,6 +13,14 @@ int main() {
     return 0;
 }
 
+int linear_search(int search_num, std::vector<int> &vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] == search_num) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 void print_search(int num, int num_index) {
     std::cout << "Number " << num << " @ index " << num_index << ".\n";
