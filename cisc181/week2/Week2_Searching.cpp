7c3653012841817895c8a4691dbc224aede2a642
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-06 19:36:55 -0700

    header comments

--- cisc181/week2/Week2_Searching.cpp
+++ cisc181/week2/Week2_Searching.cpp
@@ -14,10 +14,12 @@ int main() {
         arr[i - 1] = i;
     }
 
+    // vars needed to search for vars
     int num = 1;
     int comparisons = 0;
     int num_i = 0;
 
+    // search array
     num_i = linear_search(num, arr, ARRAY_SIZE, &comparisons);
     std::cout << "Linear Search: " << num
             << " at index: " << num_i
